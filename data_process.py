fixed weird gradients

data_process.py
@@ -2,6 +2,7 @@ import nibabel as nib
 import numpy as np
 import os, glob
 import torch
+from torch.autograd import Variable
 import pydicom
 def load_file(path, dict_key="arr_0"):
     """
@@ -77,8 +78,9 @@ def dataset_generator(paths, batch_size=1):
         yield batch_data
 
 
-def network_input(data_dir, split_tet=(0.8, 0.1, 0.1), batch_size=1):
-
+def network_input(data_dir, split_tet=(0.8, 0.1, 0.1), batch_size=1, device=None):
+    if device is None:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # create a list of files from the folder
     glob_paths = glob.glob("")
     for ext in ('*.nii', '*.nii.gz', '.npz'):
@@ -87,4 +89,4 @@ def network_input(data_dir, split_tet=(0.8, 0.1, 0.1), batch_size=1):
 
     generator = dataset_generator(paths, batch_size)
     while True:
-       yield next(generator)
+       yield Variable(next(generator).to(device), requires_grad=True)

metrics.py
@@ -22,24 +22,24 @@ def loss_mse_with_grad(outputs, atlas, grad_coef=0.00001, pixel_coef=1000):
     volumes = outputs[0]
     batch_size = volumes.shape[0]
     vector_fields = outputs[1]
-    atlas = batch_duplication(atlas, batch_size)
+    # atlas = batch_duplication(atlas, batch_size)
 
     # calculate pixel loss using MSE
     pixel_loss = pixel_coef * nn.MSELoss().forward(volumes, atlas)
 
-    # calucate the gradiants
-    unit_grid = create_unit_grid(vector_fields.shape[1:-1]).to(vector_fields.device)
-    gradiants = (unit_grid-vector_fields).abs().sum()# grad(vector_fields=vector_fields)
+    # # calucate the gradiants
+    # unit_grid = create_unit_grid(vector_fields.shape[1:-1]).to(vector_fields.device)
+    # gradiants = (unit_grid-vector_fields).abs().sum()# grad(vector_fields=vector_fields)
+    #
+    # if penalty == 'l1':
+    #     grad_loss = grad_coef*gradiants.abs().sum()
+    # else:
+    #     assert penalty == 'l2', 'penalty can only be l1 or l2. Got: %s' % penalty
+    #     grad_loss = grad_coef*(gradiants*gradiants).sum()
 
-    if penalty == 'l1':
-        grad_loss = grad_coef*gradiants.abs().sum()
-    else:
-        assert penalty == 'l2', 'penalty can only be l1 or l2. Got: %s' % penalty
-        grad_loss = grad_coef*(gradiants*gradiants).sum()
+    loss = pixel_loss #+ grad_loss
 
-    loss = pixel_loss + grad_loss
-
-    print("pixel loss {}, grad loss {}, total loss {}".format(pixel_loss, grad_loss, loss))
+    # print("pixel loss {}, grad loss {}, total loss {}".format(pixel_loss, grad_loss, loss))
 
     # normalize the loss by the batch size
     return loss / batch_size

network.py
@@ -96,7 +96,7 @@ class Type1Module(nn.Module):
     aka Voxelmorph1
     """
 
-    def __init__(self):
+    def __init__(self, atlas, device=None):
         super().__init__()
 
         self.conv_layer_down1 = self.single_conv(1, 16)
@@ -126,8 +126,22 @@ class Type1Module(nn.Module):
         # use the modules apply function to recursively apply the initialization
         self.apply(init_normal)
 
+        if atlas.type is np.ndarray:
+            self.atlas = torch.from_numpy(atlas)
+        else:
+            self.atlas = atlas
+
+        # set default device if needed
+        if device is None:
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = device
+        self.atlas = self.atlas.to(device)
+        # apply the device
+        self.unit_gird = tools.create_unit_grid(atlas.shape[2:]).to(device)
+
     def forward(self, x):
-        input_shape = x.shape
+        original_image = x
+        x = torch.cat((self.atlas, x))
         # 1: from 2 to 16 of scale 1
         conv1 = self.conv_layer_down1(x)
         x = self.maxpool(conv1)
@@ -183,11 +197,13 @@ class Type1Module(nn.Module):
         # 11: from 8+8 to 3 of scale 1
         x = torch.cat((x, conv1), dim=1)
         out = self.conv_layer_up3(x)
-        # out = 2 * torch.sigmoid(out) - 1
-
+        out = 2 * torch.sigmoid(out) - 1
         out = out.permute(0,2,3,4,1)
-        # out = out.view(input_shape[0]-1, input_shape[2], input_shape[3], input_shape[4], 3)
-        return out
+
+        vector_map = out + self.unit_gird
+        warped_image = F.grid_sample(input=original_image, grid=vector_map)
+        return warped_image, vector_map
+
 
     @staticmethod
     def single_conv(in_channels, out_channels):
@@ -248,7 +264,7 @@ class BilinearSTNRegistrator(nn.Module):
         vector_map = self.localization_net(localization_input) + self.unit_gird
         # vector_map = tools.create_unit_grid(x.shape[2:])
         # vector_map = vector_map.to(self.device)
-        warped_image = F.grid_sample(input=x, grid=vector_map,mode="nearest", padding_mode="reflection")
+        warped_image = F.grid_sample(input=x, grid=vector_map, padding_mode="reflection")
 
         return (warped_image, vector_map)
 

testing/testing_torch.py
@@ -1,6 +1,7 @@
 import torch
+import torch.nn as nn
 from tools.tools import batch_duplication
-
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 
@@ -46,24 +47,109 @@ def test_sum():
                             [[1, 2, 3], [4, 5, 6], [7, 8, -9]]]]
                           )
     print((t*t).sum())
-# t = t.repeat(2,4)
-# print(t)
 
-# t = torch.rand(12, 128, 128, 64)
-# t[0] = torch.ones(128, 128, 64)
-# t = t.repeat(2, 4)
-# print(t[0])
-# batch_size = 10
-#
-# x = torch.ones([128, 128, 64])
-# # x = x.repeat(batch_size, 1, 1, 1)
-#
-# y = torch.zeros([batch_size, 128, 128, 64])
-# y[0] = x
-#
-# atlas = torch.split(y,[1,batch_size-1])
-# z = y + atlas
-# print(z.shape)
+class Type1Module(nn.Module):
+    """
+    This is the Unet used by An Unsupervised Learning Model for
+    Deformable Medical Image Registration
+    http://openaccess.thecvf.com/content_cvpr_2018/papers/Balakrishnan_An_Unsupervised_Learning_CVPR_2018_paper.pdf
+    aka Voxelmorph1
+    """
+
+    def __init__(self, atlas, device=None):
+        super().__init__()
+
+        self.conv_layer_down1 = self.single_conv(1, 16)
+        self.conv_layer_down2 = self.single_conv(16, 32)
+        self.conv_layer_down31 = self.single_conv(32, 32)
+        self.conv_layer_down32 = self.single_conv(32, 32)
+        self.conv_layer_down33 = self.single_conv(32, 32)
+
+        self.maxpool = nn.MaxPool3d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+
+        self.conv_layer_up33 = self.single_conv(32 + 32, 32)
+        self.conv_layer_up32 = self.single_conv(32 + 32, 32)
+        self.conv_layer_up31 = self.single_conv(32 + 32, 32)
+
+        self.conv_layer_up1 = self.single_conv(32, 8)
+        self.conv_layer_up2 = self.single_conv(8, 8)
+
+        # no relu at the end
+        self.conv_layer_up3 = nn.Conv3d(in_channels=8 + 16, out_channels=3, kernel_size=3, padding=1)
+        self.atlas = atlas.to(device)
+    def forward(self, x):
+        original_image = x
+        x = torch.cat((self.atlas, x))
+        # 1: from 2 to 16 of scale 1
+        conv1 = self.conv_layer_down1(x)
+        x = self.maxpool(conv1)
+        conv1 = conv1[1:,...]  # remove atlas so skip connection will work proper
+
+
+        # 2: from 16 to 32 of scale 1/2
+        conv2 = self.conv_layer_down2(x)
+        x = self.maxpool(conv2)
+        conv2 = conv2[1:,...]  # remove atlas so skip connection will work proper
+
+
+        # 3: from 32 to 32 of scale 1/4
+        conv3 = self.conv_layer_down31(x)
+        x = self.maxpool(conv3)
+        conv3 = conv3[1:,...]  # remove atlas so skip connection will work proper
+
+
+        # # 4: from 32 to 32 of scale 1/8
+        conv4 = self.conv_layer_down32(x)
+        x = self.maxpool(conv4)
+        conv4 = conv4[1:,...]  # remove atlas so skip connection will work proper
+
+        # 5: from 32 to 32 of scale 1/16 ---- middle layer
+        x = self.conv_layer_down33(x)
+
+        # add course atlas to course batch
+        x = x[1:,...] + x[0]
+
+        # # 6: from 32+32 to 32 of scale 1/8
+        x = self.upsample(x)
+        x = torch.cat((x, conv4), dim=1)
+        x = self.conv_layer_up33(x)
+
+        # 7: from 32+32 to 32 of scale 1/4
+        x = self.upsample(x)
+        x = torch.cat((x, conv3), dim=1)
+        x = self.conv_layer_up32(x)
+
+        # 8: from 32+32 to 32 of scale 1/2
+        x = self.upsample(x)
+        x = torch.cat((x, conv2), dim=1)
+        x = self.conv_layer_up31(x)
+
+        # 9: from 32 to 8 of scale 1/2
+        x = self.conv_layer_up1(x)
+
+        # # 10: from 8 to 8 of scale 1
+        x = self.upsample(x)
+        # x = torch.cat((x, conv3), dim=1)
+        x = self.conv_layer_up2(x)
+
+        # 11: from 8+8 to 3 of scale 1
+        x = torch.cat((x, conv1), dim=1)
+        out = self.conv_layer_up3(x)
+        out = 2 * torch.sigmoid(out) - 1
+        vector_map = out.permute(0,2,3,4,1)
+
+
+        warped_image = F.grid_sample(input=original_image, grid=vector_map, padding_mode="reflection")
+        return warped_image, vector_map
+
+
+    @staticmethod
+    def single_conv(in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=False)
+        )
 
 if __name__ == "__main__":
     test_sum()

train.py
@@ -11,12 +11,12 @@ import testing.debug_tools as debug_tools
 from tensorboardX import SummaryWriter
 import skimage.transform
 import random
-
+import testing.testing_torch
 
 def train(atlas_path, train_dir, save_dir, sample_dir=None,tensorboard_dir=None, load_checkout_path=None,
           split_tet=(0.8, 0.1, 0.1),
-          images_per_epoch=100, epochs=100, learning_rate=0.001, batch_size=1,
-          save_interval=100, sample_interval=5):
+          images_per_epoch=10, epochs=100, learning_rate=0.001, batch_size=1,
+          save_interval=10, sample_interval=1):
 
     writer = SummaryWriter(os.path.join(tensorboard_dir, tools.save_string("",None)))
 
@@ -28,8 +28,8 @@ def train(atlas_path, train_dir, save_dir, sample_dir=None,tensorboard_dir=None,
         tools.set_path(tensorboard_dir)
 
     atlas = load_file_for_stn(atlas_path)*0.001
-    atlas = torch.tensor(skimage.transform.rescale(atlas,(1,1,0.25,0.25,0.25)))
-    net = Type1Module(atlas, "cuda:0")
+    # atlas = torch.tensor(skimage.transform.rescale(atlas,(1,1,0.25,0.25,0.25)))
+    net = testing.testing_torch.Type1Module(atlas, "cuda:0")
     net.cuda()
     net.train()
     if load_checkout_path is not None:
@@ -37,7 +37,7 @@ def train(atlas_path, train_dir, save_dir, sample_dir=None,tensorboard_dir=None,
 
     dataset_gen = network_input(train_dir, split_tet, batch_size)
     optimizer = optim.Adam(net.parameters(), lr=learning_rate)
-    criterion = metrics.MSE_loss
+    criterion = metrics.loss_mse_with_grad
     # writer.add_graph(net,next(dataset_gen))
     # writer.close()
     running_loss = 0.0
@@ -49,9 +49,9 @@ def train(atlas_path, train_dir, save_dir, sample_dir=None,tensorboard_dir=None,
             # -------test ---------------------
             optimizer.zero_grad()
 
-            rand_shift = random.randint(-3, 3)
-            # batch_data = debug_tools.rotate_vol(atlas, rand_shift)
-            batch_data = debug_tools.roll(atlas,2,rand_shift)
+            rand_shift = random.randint(-15, 15)
+            batch_data = debug_tools.rotate_vol(atlas, rand_shift)
+            # batch_data = debug_tools.roll(atlas,2,rand_shift)
             batch_data = batch_data.cuda()
             # ---------------------------------
             batch_data = Variable(batch_data, requires_grad=True)
@@ -60,9 +60,6 @@ def train(atlas_path, train_dir, save_dir, sample_dir=None,tensorboard_dir=None,
             loss.backward()
             optimizer.step()
             # debug_tools.plot_grad_flow(net.named_parameters())
-            # writer.add_graph(net, next(dataset_gen))
-            # writer.close()
-            # quit()
             # print statistics
             running_loss += loss.item()
 